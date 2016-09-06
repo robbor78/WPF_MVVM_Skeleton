7805ef1b2c200d965cd6ff7fd849e5a1eaa6a9cb
name <email@email.com>
2016-09-06 14:39:33 +0200

    Skeleton WPF MVVM.

---
+++ MvvmSkeletonCloset/ViewModel/EditorVM.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MvvmSkeletonCloset.ViewModel
+{
+  public class EditorVM : ObservableObject
+  {
+    public EditorVM()
+    {
+      InitProcessCommand();
+    }
+    public string Text
+    {
+      get { return text; }
+      set
+      {
+        if (value != text)
+        {
+          text = value;
+          OnPropertyChanged("Text");
+        }
+      }
+    }
+
+    public bool IsEnabled { get { return Text != null; }  }
+
+    public ICommand ProcessCommand
+    {
+      get
+      {    
+        return processCommand;
+      }
+    }
+
+    private void ProcessText()
+    {
+
+    }
+
+    private void InitProcessCommand()
+    {
+      if (processCommand == null)
+      {
+        processCommand = new RelayCommand(
+            param => ProcessText(),
+            param => true
+        );
+      }
+    }
+
+    private ICommand processCommand;
+    private string text;
+  }
+}
